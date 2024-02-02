29530ae4dedcf3c5641d8f2dc9d1c4a0562a0f0f
typohydra <74554736+typohydra@users.noreply.github.com>
2024-02-02 19:59:11 +0400

    feature/ErrorHandling
    add 404 page

--- src/routes/Error.route/Error.jsx
+++ src/routes/Error.route/Error.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+import styles from "./Error.module.scss"
+
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <main className={styles.main}>
+      <p>404</p>
+      <p>Page Not Found</p>
+      <Link to="/">Go Home</Link>
+    </main>
+  )
 }
 
 export default Error
