0b5abb0642c8560790df1de6a26425bd61db7650
typohydra <74554736+typohydra@users.noreply.github.com>
2024-02-19 19:32:53 +0400

    update hero responsiveness
    make hero responsive without the use of auto-text-size library.
    update hero layout for small screens.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import styles from "./Hero.module.scss"
-import { AutoTextSize } from "auto-text-size"
 
 const Hero = () => {
   return (
@@ -8,26 +7,23 @@ const Hero = () => {
         <div className={styles.imgContainer}>
           <img src="/assets/icons/advent.svg" />
         </div>
-        <AutoTextSize mode="box">
-          <div className={styles.text}>
-            <p>
-              Christmas is a special time. Every year, we celebrate the birth of
-              our Lord, God, and Savior Jesus Christ. After the fall of Adam and
-              Eve, the people of God were waiting for the Messiah, the chosen
-              one, who would become the king of Israel and lead them. For many
-              years, God prepared His faithful to welcome the Messiah, and one
-              night, in a place called Bethlehem, the promised one, Christ was
-              born.
-            </p>
+        <div className={styles.text}>
+          <p>
+            Christmas is a special time. Every year, we celebrate the birth of
+            our Lord, God, and Savior Jesus Christ. After the fall of Adam and
+            Eve, the people of God were waiting for the Messiah, the chosen one,
+            who would become the king of Israel and lead them. For many years,
+            God prepared His faithful to welcome the Messiah, and one night, in
+            a place called Bethlehem, the promised one, Christ was born.
+          </p>
 
-            <p>
-              The whole story of the Old Testament is about preparation to meet
-              Jesus. The narratives of Abraham, Isaac, Jacob, and Joseph tell us
-              something about Christ. On this website, you can prepare to meet
-              baby Jesus on the day of His birth.
-            </p>
-          </div>
-        </AutoTextSize>
+          <p>
+            The whole story of the Old Testament is about preparation to meet
+            Jesus. The narratives of Abraham, Isaac, Jacob, and Joseph tell us
+            something about Christ. On this website, you can prepare to meet
+            baby Jesus on the day of His birth.
+          </p>
+        </div>
       </div>
 
       <div className={styles.scroll}>
