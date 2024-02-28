92eab43e6e022d04601c568d85ae9a4e39c30295
typohydra <74554736+typohydra@users.noreply.github.com>
2024-02-28 21:09:23 +0400

    fix footer links
    fix footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div>
-        <a href="public/assets/pdf/full/illustrations.pdf" target="_blank">
+        <a href="/assets/pdf/full/illustrations.pdf" target="_blank">
           Download Illustrations
           {/* გადმოიწერეთ ილუსტრაციები */}
         </a>
-        <a href="public/assets/pdf/full/colorbook.pdf" target="_blank">
+        <a href="/assets/pdf/full/colorbook.pdf" target="_blank">
           Download Colorbook
           {/* გადმოიწერეთ გასაფერადებელი */}
         </a>
