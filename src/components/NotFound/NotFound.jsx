0097e6929d9c3876f201f71cb9635a0ffde4a3e5
typohydra <74554736+typohydra@users.noreply.github.com>
2024-03-09 16:46:28 +0400

    add translation to NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 import styles from "./NotFound.module.scss"
 
 const NotFound = () => {
+  const { t } = useTranslation()
+
   return (
     <main className={styles.main}>
       <p>404</p>
-      <p>Page Not Found</p>
-      <Link to="/">Go Home</Link>
+      <p>{t("not_found.message")}</p>
+      <Link to="/">{t("not_found.link")}</Link>
     </main>
   )
 }
